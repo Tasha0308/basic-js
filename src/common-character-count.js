764b9e09a0a3d942f638c7a822a21643ef5e8f56
Tasha0308 <tashabeliatskaya@gmail.com>
2023-04-17 15:21:45 +0200

    feat: add solution (ST) Common character count, Extended repeater

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,10 +12,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ let counter =0;
+ for(let i=0; i<s1.length; i++) {
+   if(s2.includes(s1[i])) {
+     s2=s2.replace(s1[i],'');
+     counter ++;
+   }
+ }
+ 
+return counter;
 }
-
 
 module.exports = {
   getCommonCharacterCount
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let string = String(str);
+  let arr = [];
+
+  if (options) {
+   let repeatTimes = options.repeatTimes || 1;
+   let separator = options.separator || '+';
+   let addition = String(options.addition) || '';
+   let additionRepeatTimes = options.additionRepeatTimes || 1;
+   let additionSeparator = options.additionSeparator || '|';
+
+   let addStr ="";
+    if(String(options.addition) !== 'undefined') {
+       addStr= addition + additionSeparator;
+    } else {
+       addStr = '';
+    }
+    let substr = string + addStr.repeat(additionRepeatTimes);
+
+   if (String(options.addition) !== 'undefined') {
+     substr = substr.slice(0, substr.length - additionSeparator.length);
+   }
+
+   for (let i = 0; i < repeatTimes; i++) {
+     arr.push(substr);
+   }
+   return arr.join(separator);
+ }
 }
 
 module.exports = {
